a979574397d7afded103b8eeb995187329b00be6
Rushikesh <rushi.pb03@gmail.com>
2020-12-25 19:23:01 +0530

    PartTime Employee

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,17 +1,22 @@
-public class EmpWageComputation {
-	public static void main(String[] args) {
-	System.out.println("Welcome to Employee Wage Computation");
-   	int isPresent = 1;
-	int empRatePrHr = 20;
-	int empHr = 8;
-    int empCheck=(int)Math.floor(Math.random() * 10) % 2;
-    System.out.println(empCheck);
-    if (empCheck == isPresent) {
-		System.out.println("employee is present");
-		int perDaySalary = empRatePrHr * empHr;
-		System.out.println("Employee per day salary = "+perDaySalary);
-	}
-    else
-		System.out.println("absent");
+class EmpWageComputation {
+
+	public static void main(String args[]) {
+		// Declaring Constant
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
+		//Declaring Variable
+		int empHrs = 0;
+		int empWage = 0;
+		//Computing UC
+		int empCheck = (int) ( Math.random() * 10 ) % 3;
+		if ( empCheck == IS_PART_TIME )
+			empHrs = 4;
+		else if ( empCheck == IS_FULL_TIME )
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage: " + empWage);
 	}
 }
